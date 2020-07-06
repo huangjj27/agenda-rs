a2ac53ed7b36ee7ec5c63a5d18f727735490f0e6
huangjj27 <huangjj.27@qq.com>
2020-07-06 22:53:20 +0800

    draft user

--- src/user.rs
+++ src/user.rs
@@ -1,21 +1,64 @@
-trait User {
-    fn register(n: String, p: String, em: String, pho: String) -> bool;
+use std::io;
+
+struct User {
+    name: String,
+    pass: String,
+    email: Option<String>,
+    name: Option<String>,
+}
+
+impl User {
+    fn register(n: String, p: String, em: Option<String>, pho: Option<String>) -> bool;
     fn login(n: String, p: String) -> bool;
     fn delete(n: String, p: String) -> bool;
 
-    fn name(&self) -> &str;
-    fn name_mut(&mut self) -> &mut String;
+    fn name(&self) -> &str {
+        &self.name
+    }
 
-    fn pass(&self) -> &str;
-    fn pass_mut(&mut self) -> &mut String;
+    fn set_name(&mut self, n: String) -> Result<(), io::Error> {
+        if n.len() == 0 {
+            return Err(io::ErrorKind::InValidInput, "name shouldn't be empty!");
+        }
 
-    fn email(&self) -> Option<&str>;
-    fn email(&self) -> Option<&mut String>;
+        self.name = n;
+    }
 
-    fn phone(&self) -> Option<&str>;
-    fn phone_mut(&self) -> Option<&mut String>;
+    fn pass(&self) -> &str {
+        &self.pass
+    }
+
+    fn set_pass(&mut self, p: String) -> Result<(), io::Error> {
+        if n.len() <= 6 {
+            return Err(io::ErrorKind::InValidInput, "password shouldn't be empty, or less than 6 characters!");
+        }
+
+        self.pass = p;
+    }
+
+    fn email(&self) -> Option<&str> {
+        &self.email
+    }
+
+    fn set_email(&self, em: String) {
+        if em.len() == 0 {
+            self.email = None;
+            return;
+        }
+
+        self.email = parse_email(em);
+    }
+
+    fn phone(&self) -> Option<&str> {
+        &self.phone
+    }
+
+    fn set_phone(&self, pho: String) {
+        if pho.len() == 0 {
+            self.pho = None;
+            return;
+        }
+
+        self.phone = parse_phone(pho);
+    }
 }
-
-// struct User {
-//
-// }
